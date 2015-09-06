2f5459e49284ba3290a8acdf0d47f7cb4d8b0f7f
Payne Zhou <paxibay@yahoo.ca>
2015-09-06 18:55:57 -0400

    transfers server file

---
+++ valuator/app/entities/dependancy/paxi.valuator.dependancy.settings.model.js
@@ -0,0 +1,139 @@
+﻿
+module.exports = {
+    settings: {
+        duration: {
+            period: {
+                type: Number,
+                default: 20
+            },
+            beginning: {
+                type: String,
+                default: Date.now.toString()
+            },
+            startyear: Number,
+            imcomeyear: Number,
+            normalyear: Number,
+            hasprepare: Boolean,
+            baseprice: Boolean,
+            currency: {
+                enca: {
+                    type: String,
+                    enum: ['Admin', 'Owner', 'User']
+                },
+                zhcn: {
+                    type: String,
+                    enum: ['万', '千', '百', '十']
+                }
+            }
+        },
+        evaluatingIndicator: {
+            baseIRR: Number,
+            incometax: Number,
+            surplusreserve: Number,
+            Statutorywelfarereserve: Number,
+            extractsurplusreserve: String,
+            Profitpayable: Number,
+            surplusreservescale: Number
+        }
+    },
+    priceIndex: {
+        baseYear: {
+            type: String,
+            default: '2000'
+        },
+        ppi: {
+            type: Number,
+            default: 0
+        },
+        cpi: {
+            type: Number,
+            default: 0
+        },
+        preScales: [
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String }
+        ],
+        scales: [
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String },
+            { year: String, ppi: Number, cpi: Number, edge: String }
+        ]
+    },
+};
---
+++ valuator/app/entities/valuator.model.js
@@ -0,0 +1,37 @@
+﻿
+var mongoose = require('mongoose'),
+    settings = require('./dependancy/paxi.valuator.dependancy.settings.model'),
+    blueprint = require('./dependancy/paxi.valuator.dependancy.blueprint.model'),
+    taxing = require('./dependancy/paxi.valuator.dependancy.taxing.model'),
+    costing = require('./dependancy/paxi.valuator.dependancy.costing.model')
+investing = require('./dependancy/paxi.valuator.dependancy.investing.model'),
+    lending = require('./dependancy/paxi.valuator.dependancy.lending.model'),
+    depreciation = require('./dependancy/paxi.valuator.dependancy.depreciation.model');
+
+var metadata = {
+    settings: settings,
+    blueprint: blueprint,
+    taxing: taxing,
+    costing: costing,
+    investing: investing,
+    lending: lending,
+    depreciation: depreciation
+};
+
+var reports = {};
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var ValuatorSchema = Schema({
+    user_id: {
+        type: Number,
+        ref: 'users',
+        index: true,
+        default: '100000'
+    },
+    metadata: metadata,
+    reports: reports
+});
+
+mongoose.model('Valuator', ValuatorSchema);
